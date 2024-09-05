c7b9d9475e5ad10619d81e15bc630d30bb6827c5
Evan Sizemore <evan@wool.homes>
2024-09-05 13:08:21 -0400

    Make sure we remove formatted code folders

--- formatted-code/formatted-code-black/apps/accounts/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-# Create your tests here.
-class AccountsTestCase(TestCase):
-    def test_something(self):
-        pass
--- formatted-code/formatted-code-isort/apps/core/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-# Create your tests here.
-class CoreTestCase(TestCase):
-    def test_something(self):
-        pass
